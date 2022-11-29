fd629e8377609bae5cc18abe7be5033ed3b7758b
Brice Pelle <onlybakam@gmail.com>
2022-11-29 15:39:39 -0800

    baseline

---
+++ app/pages/index.tsx
@@ -0,0 +1,97 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import appsyncImg from '../public/appsync.jpg'
+
+
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+// const user = {
+//   name: 'Tom Cook',
+//   email: 'tom@example.com',
+//   imageUrl:
+//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// }
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+]
+const userNavigation: any[] = []
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <nav className="bg-white border-b border-gray-200">
+              <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16">
+                  <div className="flex">
+                    <div className="flex items-center flex-shrink-0">
+                      <Image src={appsyncImg}
+                        className="block w-auto h-8 lg:hidden"
+                        alt="appsync"
+                      />
+                      <Image src={appsyncImg}
+                        className="hidden w-auto h-8 lg:block"
+                        alt="appsync"
+                      />
+                    </div>
+                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'border-indigo-500 text-gray-900'
+                              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                            'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                 
+                  
+                </div>
+              </div>
+        </nav>
+
+        <div className="py-10">
+          <header>
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              {/* Replace with your content */}
+              <div className="px-4 py-8 sm:px-0">
+                <div className="border-4 border-gray-200 border-dashed rounded-lg h-96" />
+              </div>
+              {/* /End replace */}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
+
